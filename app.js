3fed243b0333840aa75c09dd37558e900201079e
Fernando Shibata <shibata.421@gmail.com>
2021-02-02 19:47:55 -0300

    Adds two new routes

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ app.get("/", (req, res) => {
     res.send("Hello from our web app!!");
 })
 
+app.get("/joke", (req, res) => {
+    const joke = "What do you call a dog that does magic tricks? A labracadabrador.";
+    res.send(joke);
+})
+
+app.get("/random_num", (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.send("Your lucky number is " + num);
+})
+
 app.listen(port, () => {
     console.log('App listening on port ' + port)
 })
