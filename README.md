8b3c23c76f73fd78ab3676aa8802771e460ac3f9
Fernando Shibata <shibata.421@gmail.com>
2021-02-02 20:18:22 -0300

    Adds connection to MySQL database and asks for total users

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## Shin's MySQL Bootcamp Course Repository
 This is my repository for the MySQL Bootcamp course from Udemy <br>
-https://www.udemy.com/course/the-ultimate-mysql-bootcamp-go-from-sql-beginner-to-expert/
+https://www.udemy.com/course/the-ultimate-mysql-bootcamp-go-from-sql-beginner-to-expert/<br>
+<br>
+To use it, create a user.json file with host, username, password and database to access your MySQL
 
 ```bash
 npm run dev
--- app.js
+++ app.js
@@ -1,21 +1,37 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
+const mysql = require("mysql");
+
+const userData = JSON.parse(fs.readFileSync("./user.json"));
 const app = express();
 const port = 3000;
 
+const connection = mysql.createConnection({
+  host: userData.host,
+  user: userData.username,
+  password: userData.password,
+  database: userData.database
+});
+
 app.get("/", (req, res) => {
-    res.send("Hello from our web app!!");
-})
+    const q = "SELECT COUNT(*) as total FROM users";
+    connection.query(q, (err, results) => {
+        if(err) { throw err }
+        res.send("We have " + results[0].total + " users");
+    })
+});
 
 app.get("/joke", (req, res) => {
-    const joke = "What do you call a dog that does magic tricks? A labracadabrador.";
-    res.send(joke);
-})
+  const joke =
+    "What do you call a dog that does magic tricks? A labracadabrador.";
+  res.send(joke);
+});
 
 app.get("/random_num", (req, res) => {
-    const num = Math.floor(Math.random() * 10) + 1;
-    res.send("Your lucky number is " + num);
-})
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.send("Your lucky number is " + num);
+});
 
 app.listen(port, () => {
-    console.log('App listening on port ' + port)
-})
+  console.log("App listening on port " + port);
+});
